b125276671ee1bfded79210a112620109f711c66
Sachi-0612 <sachikaursidhu006@gmail.com>
2025-09-07 14:54:11 +0530

    second commit

--- index.js
+++ index.js
@@ -1,9 +1,47 @@
 // console.log('Hello Bonjour')
 
 require('dotenv').config()
-const express = require('express')
+
+const express = require('express')// import express from "express"
+
 const app = express()
 const port = 3000
+
+const github = {
+  "login": "hiteshchoudhary",
+  "id": 11613311,
+  "node_id": "MDQ6VXNlcjExNjEzMzEx",
+  "avatar_url": "https://avatars.githubusercontent.com/u/11613311?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/hiteshchoudhary",
+  "html_url": "https://github.com/hiteshchoudhary",
+  "followers_url": "https://api.github.com/users/hiteshchoudhary/followers",
+  "following_url": "https://api.github.com/users/hiteshchoudhary/following{/other_user}",
+  "gists_url": "https://api.github.com/users/hiteshchoudhary/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/hiteshchoudhary/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/hiteshchoudhary/subscriptions",
+  "organizations_url": "https://api.github.com/users/hiteshchoudhary/orgs",
+  "repos_url": "https://api.github.com/users/hiteshchoudhary/repos",
+  "events_url": "https://api.github.com/users/hiteshchoudhary/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/hiteshchoudhary/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": "Hitesh Choudhary",
+  "company": null,
+  "blog": "https://hitesh.ai",
+  "location": "India",
+  "email": null,
+  "hireable": null,
+  "bio": "I make coding videos on youtube and for courses. My youtube channel explains my work more. Check that out",
+  "twitter_username": "hiteshdotcom",
+  "public_repos": 113,
+  "public_gists": 5,
+  "followers": 49129,
+  "following": 0,
+  "created_at": "2015-03-23T13:03:25Z",
+  "updated_at": "2025-09-02T19:53:50Z"
+}
 
 app.get('/', (req, res)=>{
     res.send('Hello World')
@@ -12,10 +50,14 @@ app.get('/', (req, res)=>{
 app.get('/youtube', (req, res)=>{
     res.send('Bonjour')
 })
+app.listen(port, ()=> {
+    console.log(`Example app listening on port ${port}`)
+})
 
 app.listen(process.env.PORT, ()=> {
     console.log(`Example app listening on port ${process.env.PORT}`)
 })
-app.listen(port, ()=> {
-    console.log(`Example app listening on port ${port}`)
+
+app.get('/github',(req,res) => {
+    res.json(github)
 })
